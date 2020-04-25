231818935f6faf70878578af21d09b0d1a8290c0
Zelosin <thezelosin@gmail.com>
2020-04-25 20:30:35 +0300

    download template fix

--- src/main/resources/static/js/vue/settings/file-settings.js
+++ src/main/resources/static/js/vue/settings/file-settings.js
@@ -2,21 +2,10 @@ fileSettings = new Vue({
    el: '#downloadLink',
    methods: {
        downloadTemplate: function(){
-           fetch('/transfer/configuration?type=sample')
-               .then(resp => resp.blob())
-               .then(blob => {
-                   const url = window.URL.createObjectURL(blob);
-                   const a = document.createElement('a');
-                   a.style.display = 'none';
-                   a.href = url;
-                   // the filename you want
-                   a.download = 'template.xml';
-                   document.body.appendChild(a);
-                   a.click();
-                   window.URL.revokeObjectURL(url);
-                   alert('your file has downloaded!'); // or you know, something with better UX...
-               })
-               .catch(() => alert('oh no!'));
+           var link = document.createElement("a");
+           link.download = name;
+           link.href = "/transfer/configuration?type=sample";
+           link.click();
        }
    }
 });
